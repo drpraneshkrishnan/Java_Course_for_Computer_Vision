3cc5cf674bfd303caa76242b3dac9743d5fbc916
LAPTOP-FJTG3P2R\Prenesh.Krishnan <pranesh.krishnan@skymind.my>
2021-01-03 02:12:17 +0800

    Initial commit

---
+++ lesson1/task1/src/Task.java
@@ -0,0 +1,11 @@
+public class Task {
+  //put your task here
+  public static void main(String[] args) {
+      int t =5;
+      System.out.print("Test");
+      System.out.println("");
+      System.out.println("How is this possible");
+      System.out.print("Test");
+      System.out.println(t);
+  }
+}
